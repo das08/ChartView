511986d4e60801ab56e9b07e3526fa7c087ee818
das08 <takeda5955@gmail.com>
2021-02-07 13:31:44 +0900

    changed pos

--- Sources/SwiftUICharts/LineChart/LineView.swift
+++ Sources/SwiftUICharts/LineChart/LineView.swift
@@ -58,7 +58,7 @@ public struct LineView: View {
                             .font(.callout)
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
                     }
-                }
+                }.offset(x: 0, y: 20)
                 ZStack {
                     GeometryReader { reader in
                         Rectangle()
